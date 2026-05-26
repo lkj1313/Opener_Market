60ad3d0357effd9966a49d82b108049e6bbfe662
lkj1313 <smm45108@gmail.com>
2026-05-26 20:55:50 +0900

    feat(api): Super Admin Seed 및 공용 타입 패키지 설정

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,1 @@
+export * from './types/auth/user-payload.type.js';
---
+++ packages/shared/src/types/auth/user-payload.type.ts
@@ -0,0 +1,7 @@
+export type UserRole = 'BUYER' | 'SELLER' | 'ADMIN';
+
+export interface UserPayload {
+  userId: string;
+  email: string;
+  role: UserRole;
+}
